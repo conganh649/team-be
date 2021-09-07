81d2bd185230468e78747c5e5139a9e6f57c9444
conganh649 <tranconganh1999@gmail.com>
2021-09-07 15:07:04 +0700

    Add basic CRUD user

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -6,10 +6,10 @@ const userSchema = new mongoose.Schema(
   {
     userName: { type: String, required: true, trim: true },
     fullName: { type: String, required: true },
-    phoneNumber: { type: String, required: true },
+    phoneNumber: { type: String },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    address: { type: String, required: true },
+    address: { type: String },
     dateofbirth: { type: Date, required: true, default: Date.now },
     role: {
       type: String,
