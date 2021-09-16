5e733be2247921994493084f7ae0f160826a66b2
conganh649 <tranconganh1999@gmail.com>
2021-09-16 13:46:45 +0700

    Resolve conflict

--- server/routes/router.js
+++ server/routes/router.js
@@ -2,19 +2,12 @@ const express = require("express");
 const route = express.Router();
 
 const userController = require("../controller/userController");
-const productController = require("../controller/productController");
 const authController = require("../controller/authController");
 // API
 route.post("/api/users", userController.create);
 route.get("/api/users", userController.find);
 route.put("/api/users/:id", userController.update);
 route.delete("/api/users/:id", userController.delete);
-
-route.get("/api/products", productController.getAll);
-route.post("/api/product", productController.create);
-route.get("/api/product/:id", productController.getOne);
-route.put("/api/product/:id", productController.update);
-route.delete("/api/products/:id", productController.delete);
 
 //AUTH
 route.post("/api/signup", authController.signup);
