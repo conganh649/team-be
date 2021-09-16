93e2db8679efa36826ec80201c9aee78a037a16b
conganh649 <tranconganh1999@gmail.com>
2021-09-16 15:08:14 +0700

    Fix email verification

--- server/routes/router.js
+++ server/routes/router.js
@@ -3,6 +3,7 @@ const route = express.Router();
 
 const userController = require("../controller/userController");
 const authController = require("../controller/authController");
+const productController = require("../controller/productController");
 // API
 
 // USERS
@@ -10,6 +11,13 @@ route.post("/api/users", userController.create);
 route.get("/api/users", userController.find);
 route.put("/api/users/:id", userController.update);
 route.delete("/api/users/:id", userController.delete);
+
+// PRODUCTS
+route.get("/api/products", productController.getAll);
+route.post("/api/product", productController.create);
+route.get("/api/product/:id", productController.getOne);
+route.put("/api/product/:id", productController.update);
+route.delete("/api/products/:id", productController.delete);
 
 //AUTH
 route.post("/api/signup", authController.signup);
