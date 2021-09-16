684bf2aff5499be0aa4ba4cee1cdb1c13e83b554
conganh649 <tranconganh1999@gmail.com>
2021-09-16 13:26:37 +0700

    Add signup API, unit test

--- server/routes/router.js
+++ server/routes/router.js
@@ -2,11 +2,18 @@ const express = require("express");
 const route = express.Router();
 
 const userController = require("../controller/userController");
+const authController = require("../controller/authController");
 
 // API
+
+//USERS
 route.post("/api/users", userController.create);
 route.get("/api/users", userController.find);
 route.put("/api/users/:id", userController.update);
 route.delete("/api/users/:id", userController.delete);
+
+//AUTH
+route.post("/api/signup", authController.signup);
+route.get("/api/activation/:token", authController.activateAccount);
 
 module.exports = route;
