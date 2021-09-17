0163b0107523548f8615abb4683c51d014403375
thucong <51808157+thucong@users.noreply.github.com>
2021-09-17 09:31:00 +0700

    move route to router.js

--- server/routes/router.js
+++ server/routes/router.js
@@ -4,6 +4,8 @@ const route = express.Router();
 const userController = require("../controller/userController");
 const authController = require("../controller/authController");
 const productController = require("../controller/productController");
+const cartController = require("../controller/cartController");
+const labelController = require("../controller/labelController");
 // API
 
 // USERS
@@ -22,5 +24,37 @@ route.delete("/api/products/:id", productController.delete);
 //AUTH
 route.post("/api/signup", authController.signup);
 route.get("/api/activation/:token", authController.activateAccount);
+
+// CARTS
+route.post("/carts", async (req, res) => {
+  if (Array.isArray(req.body.items)) {
+    return cartController.createCart(req, res);
+  }
+  return await cartController.createCart(req, res);
+});
+route.get("/carts", (req, res) => cartController.getCartItem(req, res));
+route.delete("/carts/:id", (req, res) => {
+  return cartController.deleteCartItem(req, res);
+});
+route.put("/carts/:id", (req, res) => {
+  return cartController.updateCartItem(req, res);
+});
+route.get("/carts/:id", (req, res) => {
+  cartController.getCartItemById(req, res);
+});
+
+//LABELS
+route.post("/labels", async (req, res) => {
+  if (Array.isArray(req.body)) {
+    return labelController.createLabel(req, res);
+  }
+  return await labelController.createLabel(req, res);
+});
+route.get("/labels", (req, res) => labelController.getLabel(req, res));
+route.get("/labels/:id", (req, res) => labelController.getLabelById(req, res));
+route.put("/labels/:id", (req, res) => labelController.updateLabel(req, res));
+route.delete("/labels/:id", (req, res) =>
+  labelController.deleteLabel(req, res)
+);
 
 module.exports = route;
--- server/server.js
+++ server/server.js
@@ -4,13 +4,12 @@ const bodyParser = require("body-parser");
 var cors = require("cors");
 
 var users = require("./routes/router");
+const cartRoutes = require("./routes/router");
+const labelRoutes = require("./routes/router");
 
 const dotenv = require("dotenv");
 dotenv.config();
-//routes
-const cartRoutes = require("./routes/api/cart");
 
-const labelRoutes = require("./routes/api/label");
 var app = express();
 app.use(express.json());
 app.use(bodyParser.json());
@@ -30,8 +29,8 @@ const port = process.env.PORT || 3002;
 app.listen(port, () => {
   console.log(`Server Running at ${port}`);
 });
-// cart route
-app.use("/api", cartRoutes);
 
+app.use("/api", cartRoutes);
 app.use("/api", labelRoutes);
+
 module.exports = app;
