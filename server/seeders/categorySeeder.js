03e841f1f03bf4566c3934af678a6cd6157a772e
conganh649 <tranconganh1999@gmail.com>
2021-09-28 11:31:02 +0700

    Add data seeder

---
+++ server/seeders/categorySeeder.js
@@ -0,0 +1,36 @@
+"use-strict";
+const excelToJson = require("convert-excel-to-json");
+const connectDB = require("../connection/connection");
+var Category = require("../models/categoryModel");
+const args = process.argv;
+async function connect() {
+  await connectDB();
+}
+
+const result = excelToJson({
+  sourceFile:
+    "D:/Working/Enclave/Technical_Training/team-project/be/server/seeders/data.xlsx",
+  sheets: [
+    {
+      name: "categories",
+      header: { rows: 1 },
+      columnToKey: {
+        A: "categoryName",
+      },
+    },
+  ],
+});
+
+async function seedData() {
+  await connect();
+  await Category.remove({});
+  for (i = 0; i < args[2]; i++) {
+    const newCategory = new Category({
+      categoryName: result.categories[i].categoryName,
+    });
+    newCategory.save(newCategory);
+    console.log(i + "-" + newCategory.categoryName + " saved");
+  }
+}
+
+seedData();
---
+++ server/seeders/userSeeder.js
@@ -0,0 +1,50 @@
+"use-strict";
+const excelToJson = require("convert-excel-to-json");
+const connectDB = require("../connection/connection");
+var User = require("../models/userModel");
+const args = process.argv;
+async function connect() {
+  await connectDB();
+}
+
+const result = excelToJson({
+  sourceFile:
+    "D:/Working/Enclave/Technical_Training/team-project/be/server/seeders/data.xlsx",
+  sheets: [
+    {
+      name: "users",
+      header: { rows: 1 },
+      columnToKey: {
+        A: "userName",
+        B: "fullName",
+        C: "phoneNumber",
+        D: "email",
+        E: "password",
+        F: "address",
+        G: "dateofbirth",
+        H: "role",
+      },
+    },
+  ],
+});
+
+async function seedData() {
+  await connect();
+  await User.remove({});
+  for (i = 0; i < args[2]; i++) {
+    const newUser = new User({
+      userName: result.users[i].userName,
+      fullName: result.users[i].fullName,
+      phoneNumber: result.users[i].phoneNumber,
+      email: result.users[i].email,
+      password: result.users[i].password,
+      address: result.users[i].address,
+      dateofbirth: result.users[i].dateofbirth,
+      role: result.users[i].role,
+    });
+    newUser.save(newUser);
+    console.log(i + "-" + newUser.userName + " saved");
+  }
+}
+
+seedData();
